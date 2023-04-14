94928c9445151225e2d81e050f40da757e9eec83
Pratik Chapke <82867372+PratikChapke@users.noreply.github.com>
2023-04-15 01:03:44 +0530

    Update option.js

--- routes/option.js
+++ routes/option.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const option = require('../controllers/OptionController')
 
-//  router.get('/', option.index )
+ router.get('/', option.index )
 
-// router.post('/:id/options/create',option.createOption )
-// router.delete('/:id/delete', option.optionDelete);
-// router.get('/:id/add_vote', option.addVote)
+ router.post('/:id/options/create',option.createOption )
+router.delete('/:id/delete', option.optionDelete);
+ router.get('/:id/add_vote', option.addVote)
 
 module.exports = router;
